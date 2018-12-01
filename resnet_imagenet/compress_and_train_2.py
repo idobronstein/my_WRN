25718afd54884ab1067c2c33f2147af9e76082e9
idobronstein <idobron123@gmail.com>
2018-12-01 16:10:40 +0200

    compress and train

--- resnet_imagenet/compress_and_train_2.py
+++ resnet_imagenet/compress_and_train_2.py
@@ -26,7 +26,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 
-UPDATE_PARAM_REGEX = re.compile('(group)(3)(/group3.block)(\d)(.conv1/kernel:0)')
+UPDATE_PARAM_REGEX = re.compile('(group)(0)(/group0.block)(\d)(.conv1/kernel:0)')
 CONV1_KERNEL1_NAME = 'group{group_num}.block{block_num}.conv1.weight'
 CONV1_KERNEL2_NAME = 'group{group_num}.block{block_num}.conv2.weight'
 CONV1_BIAS_NAME = 'group{group_num}.block{block_num}.conv1.bias'
@@ -276,6 +276,7 @@ def compress():
             if step % FLAGS.test_interval == 0:
                 test_loss, test_acc = 0.0, 0.0
                 for i in  range(FLAGS.test_iter):
+                    import ipdb; ipdb.set_trace()
                     test_images_val, test_labels_val = next(test_loader)
                     print(test_labels_val)
                     loss_value, acc_value = sess.run([new_network.loss, new_network.acc],
