834a2c0bfb058abf28833cf8838e78b22ab4e1f6
idobronstein <idobron123@gmail.com>
2018-12-24 07:29:16 +0200

    test

--- resnet_imagenet/compress_and_train_2.py
+++ resnet_imagenet/compress_and_train_2.py
@@ -286,6 +286,8 @@ def compress():
             image_train_file = 0
             index_train_file = 0
             for step in range(init_step, max_steps):
+                if step == 20000:
+                    import ipdb; ipdb.set_trace()
                 # Test
                 if step % FLAGS.test_interval == 0:
                     test_loss, test_acc = 0.0, 0.0
