d552eed8950344354bbcfe0e714838fd3319a533
idobronstein <idobron123@gmail.com>
2019-01-11 19:23:04 +0200

    fix

--- resnet_imagenet/compress_and_train_2.py
+++ resnet_imagenet/compress_and_train_2.py
@@ -307,7 +307,6 @@ def compress():
                     if step % FLAGS.test_interval == 0:
                         test_loss, test_acc = 0.0, 0.0
                         for i in  range(FLAGS.test_iter):
-                            import pdb; pdb.set_trace()
                             test_images_val, test_labels_val, new_test_loader = get_next_batch(test_loader, 'val', True)
                             if new_test_loader is not None:
                                 test_loader = new_test_loader
@@ -357,9 +356,7 @@ def compress():
                         checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
                         saver.save(sess, checkpoint_path, global_step=step)
                 test_loss, test_acc = 0.0, 0.0
-                pdb.set_trace()
                 for i in  range(FLAGS.final_test_iter):
-                    print(i)
                     test_images_val, test_labels_val, new_test_loader = get_next_batch(test_loader, 'val', True)
                     if new_test_loader is not None:
                         test_loader = new_test_loader
